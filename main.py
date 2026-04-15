59a7c49427447e1c63785a1ab57c0959bbe2ac89
babywka95-hue <babywka95@gmail.com>
2026-04-15 16:21:10 +0300

    main.py

--- main.py
+++ main.py
@@ -1,15 +1,23 @@
+import os
 from telegram import Update
 from telegram.ext import ApplicationBuilder, MessageHandler, filters, ContextTypes
-import os
 
 TELEGRAM_TOKEN = os.getenv("8665178501:AAHR4Asen0W9r3neZJn1Ll6fXZEQSvoApJo")
 
-async def test(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("✅ БОТ ЖИВОЙ. КОД ОБНОВЛЁН И РАБОТАЕТ")
+async def test_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    """
+    Просто отвечает на любое сообщение.
+    Это проверка, что бот живой.
+    """
+    await update.message.reply_text("✅ БОТ ЖИВОЙ! Render Background Worker работает.")
 
 def main():
     app = ApplicationBuilder().token(TELEGRAM_TOKEN).build()
-    app.add_handler(MessageHandler(filters.ALL, test))
+
+    # реагирует на все сообщения (текст, фото, стикеры)
+    app.add_handler(MessageHandler(filters.ALL, test_message))
+
+    print("Bot started")
     app.run_polling()
 
 if __name__ == "__main__":
