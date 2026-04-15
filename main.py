133c66f14766d0c35a4dd60cb03b57a058d33dc0
babywka95-hue <babywka95@gmail.com>
2026-04-15 18:05:28 +0300

    main.py

--- main.py
+++ main.py
@@ -4,11 +4,14 @@ from telegram import Bot, Update
 from telegram.ext import Application, CommandHandler, ContextTypes
 
 TOKEN = os.getenv("BOT_TOKEN")
+WEBHOOK_URL = os.getenv("WEBHOOK_URL")
 
 app = FastAPI()
 bot = Bot(token=TOKEN)
 
 application = Application.builder().token(TOKEN).build()
+
+started = False
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("Бот работает 🚀")
@@ -23,13 +26,15 @@ def home():
 
 @app.post("/")
 async def webhook(request: Request):
+    global started
+
+    if not started:
+        await application.initialize()
+        await application.bot.set_webhook(WEBHOOK_URL)
+        started = True
+
     data = await request.json()
     update = Update.de_json(data, bot)
     await application.process_update(update)
+
     return {"ok": True}
-
-
-@app.on_event("startup")
-async def startup():
-    await application.initialize()
-    await bot.set_webhook(os.getenv("WEBHOOK_URL"))
