fb296784fe7b1dbde17558b6e7eab3408eaad00f
babywka95-hue <babywka95@gmail.com>
2026-04-15 17:31:58 +0300

    main.py

--- main.py
+++ main.py
@@ -1,16 +1,24 @@
 import os
+import asyncio
 from telegram import Update
-from telegram.ext import ApplicationBuilder, MessageHandler, filters, ContextTypes
+from telegram.ext import Application, CommandHandler, ContextTypes
 
-TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
+TOKEN = os.getenv("BOT_TOKEN")
 
-print("TOKEN:", TELEGRAM_TOKEN)
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("Бот запущен 🚀")
 
-async def handle(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("✅ Бот работает!")
+async def main():
+    app = Application.builder().token(TOKEN).build()
 
-app = ApplicationBuilder().token(TELEGRAM_TOKEN).build()
-app.add_handler(MessageHandler(filters.ALL, handle))
+    app.add_handler(CommandHandler("start", start))
 
-print("🚀 Bot started")
-app.run_polling()
+    print("Bot started")
+
+    await app.initialize()
+    await app.start()
+    await app.updater.start_polling()
+    await app.updater.idle()
+
+if __name__ == "__main__":
+    asyncio.run(main())
