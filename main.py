7e6928b9766d0512eb5e0ef8f1c5245809d6395e
babywka95-hue <babywka95@gmail.com>
2026-04-15 18:32:23 +0300

    main.py

--- main.py
+++ main.py
@@ -4,17 +4,11 @@ from telegram.ext import Application, CommandHandler, ContextTypes
 
 TOKEN = os.getenv("BOT_TOKEN")
 
-if not TOKEN:
-    raise Exception("BOT_TOKEN is missing")
-
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("Бот работает 🚀")
 
 def main():
     app = Application.builder().token(TOKEN).build()
-
-    # 🔥 УБИРАЕМ КОНФЛИКТЫ С ДРУГИМИ СЕССИЯМИ
-    app.bot.delete_webhook(drop_pending_updates=True)
 
     app.add_handler(CommandHandler("start", start))
 
