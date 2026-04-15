61a82978bbed79f39e88e8822021485e8827f439
babywka95-hue <babywka95@gmail.com>
2026-04-15 18:12:14 +0300

    main.py

--- main.py
+++ main.py
@@ -1,43 +1,23 @@
 import os
-from fastapi import FastAPI, Request
-from telegram import Bot, Update
+from telegram import Update
 from telegram.ext import Application, CommandHandler, ContextTypes
 
 TOKEN = os.getenv("BOT_TOKEN")
-WEBHOOK_URL = os.getenv("WEBHOOK_URL")
 
-app = FastAPI()
-bot = Bot(token=TOKEN)
-application = Application.builder().token(TOKEN).build()
-
-started = False
+if not TOKEN:
+    raise Exception("BOT_TOKEN is not set")
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("Бот работает 🚀")
 
-application.add_handler(CommandHandler("start", start))
+def main():
+    app = Application.builder().token(TOKEN).build()
 
+    app.add_handler(CommandHandler("start", start))
 
-@app.get("/")
-def home():
-    return "Bot is alive"
+    print("Bot started")
 
+    app.run_polling()
 
-@app.post("/")
-async def webhook(request: Request):
-    global started
-
-    if not started:
-        await application.initialize()
-
-        # 🔥 ВАЖНАЯ ЗАЩИТА
-        if WEBHOOK_URL:
-            await application.bot.set_webhook(WEBHOOK_URL)
-
-        started = True
-
-    data = await request.json()
-    update = Update.de_json(data, bot)
-    await application.process_update(update)
-
-    return {"ok": True}
+if __name__ == "__main__":
+    main()
