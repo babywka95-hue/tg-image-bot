a2024301d07a2b208ad7e24b50337237f779f2c3
babywka95-hue <babywka95@gmail.com>
2026-04-16 15:59:25 +0300

    main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 import os
 
 print("ENV KEYS:", list(os.environ.keys()))
-print("TOKEN RAW:", os.environ.get("TELEGRAM_TOKEN"))
+print("TOKEN RAW:", os.environ.get("8665178501:AAHR4Asen0W9r3neZJn1Ll6fXZEQSvoApJo"))
